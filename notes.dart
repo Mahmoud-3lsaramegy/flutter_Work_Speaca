eca39540c941875feb30e0b229fcc7f1f0c11ca2
Mahmoud-3lsaramegy <elsaramegymahmoud404@gmail.com>
2023-08-21 02:51:16 +0300

    add note

---
+++ notes.dart
@@ -0,0 +1,49 @@
+/// row & wrap solve erorr 
+
+
+//////
+///Flutter from 0 to profional 
+//////////////////
+/// wael Abo hamza
+/////////////////
+/*
+Dart 
+
+1 - https://www.youtube.com/playlist?list=PL93xoMrxRJIsYc9L0XBSaiiuq01JTMQ_o
+
+Flutter
+ 
+2 - https://www.youtube.com/playlist?list=PL93xoMrxRJIvtIXjAiX15wcyNv-LOWZa9
+
+Firebase 
+
+3 - https://www.youtube.com/playlist?list=PL93xoMrxRJIvHhxhB21YzzeimEEzzAz6g
+
+Provider 
+
+4 - https://www.youtube.com/playlist?list=PL93xoMrxRJIviJiC76oO5aV8bDp2s3OrA
+
+Getx 
+
+5 - https://www.youtube.com/playlist?list=PL93xoMrxRJIvZHL420f63bWIOrcoM6NU-
+
+Sql 
+
+6 - https://www.youtube.com/playlist?list=PL93xoMrxRJIuicqcd1UpFUYMfWKGp7JmI
+
+Sqflite
+
+7 -  https://www.youtube.com/playlist?list=PL93xoMrxRJIvPg3nW8WG0HDDgpMQtUkag
+
+Rest Api With PHP And Flutter 
+
+8 - https://www.youtube.com/playlist?list=PL93xoMrxRJItcqJJgyCpA7Wv_YL-ii6Dl
+
+Ecommerce App 
+
+9 - https://www.youtube.com/playlist?list=PL93xoMrxRJIuvTINf31EZ4S06lPklY-r0
+
+Package Of The Week 
+
+10 - https://www.youtube.com/playlist?list=PL93xoMrxRJIuIpCMr4qi2XkOzdEX7PlrU
+*/
